21a9e76f898dc3dafbb4912305fcfd919e89a69c
Krisk <31748409+BubblyGumKrisk@users.noreply.github.com>
2022-05-02 09:59:33 -0700

    Add files via upload

---
+++ server.py
@@ -0,0 +1,8 @@
+from websocket_server import WebsocketServer
+
+def new_client(client, server):
+	server.send_message_to_all("Hey all, a new client has joined us")
+
+server = WebsocketServer(host='127.0.0.1', port=8080)
+server.set_fn_new_client(new_client)
+server.run_forever()
