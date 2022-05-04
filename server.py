a5272a00e2d628d547c447f7488f1d6c78fb00af
Krisk <31748409+BubblyGumKrisk@users.noreply.github.com>
2022-05-04 09:10:41 -0700

    Update server.py

--- server.py
+++ server.py
@@ -1,8 +1,18 @@
 from websocket_server import WebsocketServer
 
-def new_client(client, server):
-	server.send_message_to_all("Hey all, a new client has joined us")
+GET = str({"mode": "GET", "data": None})
 
-server = WebsocketServer(host='127.0.0.1', port=8080)
-server.set_fn_new_client(new_client)
-server.run_forever()
+def get_msg(client, wss, msg):
+	wss.send_message_to_all(GET)
+	for c in wss.clients:
+		if not (client["id"] == c["id"]):
+			wss.send_message(c, str({"mode": "POST", "data": eval(msg)}))
+			
+def on_new(client, wss):
+	print("new")
+	wss.send_message(client, GET)
+	
+wss = WebsocketServer(host='127.0.0.1', port=8080)
+wss.set_fn_message_received(get_msg)
+wss.set_fn_new_client(on_new)
+wss.run_forever()
